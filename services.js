a8d0a82a243af389c0f301e06ed49e11a6a8f0db
Adriane <asemp055@uottawa.cagit config --global user.email>
2025-06-03 16:49:05 -0400

    Description de chacune des taches et structure de l'application

---
+++ services.js
@@ -0,0 +1,19 @@
+/**
+ * DONNÉES : LISTE DES SERVICES (Adriane & Marie)
+ * Rôle : Centraliser les infos sur les services offerts
+ * À faire :
+ * - Ajouter tous les services (nom, prix, durée)
+ * - Exemple : 
+ *   { id: 1, name: "Box Braids", price: 120, duration: "3h" }
+ * - Garder les prix cohérents !
+ */
+export const services = [
+  {
+    id: 1,
+    name: "Tresses collées (Box Braids)",
+    price: 120,
+    duration: "3h",
+    description: "Tresses avec extensions synthétiques"
+  },
+  // À compléter avec Marie
+];
