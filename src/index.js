9d2a75a7a5092df923ec37aa2e7d44d78ec5c28e
Mhyoula <145516100+Mhyoula@users.noreply.github.com>
2025-06-04 21:32:00 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ import App from './App';
 // Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// Vos styles personnalisés
+// Vos styles CSS
 import './styles/styles.css';
+import './styles/Header.css';
+import './styles/home.css';
 import './styles/services.css';
 import './styles/bookingModal.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
